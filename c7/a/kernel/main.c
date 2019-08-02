f5f585c6ee95d73aa153a14158e2e952802e00d9
liyi <1020076136@qq.com>
2019-08-03 07:14:24 +0800

    first interrupt

---
+++ c7/a/kernel/main.c
@@ -0,0 +1,51 @@
+#include "print.h"
+#include "init.h"
+void main(){
+put_char('\n');	
+put_char('\n');	
+put_char('\n');	
+put_char('n');	
+put_char('e');	
+put_char('\b');
+put_char('l');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+put_char('i');	
+print_str("hello world!\n  present by yleeeee\na simple operation system kernel!\n");
+put_int(1);
+put_char('\n');
+put_int(0x0001234);
+put_char('\n');
+init_all();
+asm volatile ("sti");
+while(1);
+}
+
