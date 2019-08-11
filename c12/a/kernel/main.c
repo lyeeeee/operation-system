fcd617538ca72757e352d85b116869dc43b9455e
liyi <1020076136@qq.com>
2019-08-11 12:50:06 +0800

    syscall

--- c12/a/kernel/main.c
+++ c12/a/kernel/main.c
@@ -6,29 +6,29 @@
 #include "keyboard.h"
 #include "ioqueue.h"
 #include "process.h"
+#include "syscall-init.h"
+#include "syscall.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
 void u_prog_a(void);
 void u_prog_b(void);
-int test_var_a = 0,test_var_b = 0;
+int prog_a_pid = 0,prog_b_pid = 0;
 int main(void) {
    put_str("I am kernel\n");
    init_all();
 
-   //thread_start("cosumer_a", 31, k_thread_a, " A:");
-   //thread_start("cnsumer_b", 8, k_thread_b, " B:");
    process_execute(u_prog_a, "user_prog_a");
-   //process_execute(u_prog_b, "user_prog_b");
+   process_execute(u_prog_b, "user_prog_b");
+   int i;
+   for(i = 0;i < 10000;++i){
+   
+   }
+   thread_start("cosumer_a", 31, k_thread_a, " A:");
+   thread_start("cnsumer_b", 8, k_thread_b, " B:");
    
 
    intr_enable();	// 打开中断,使中断起作用
-   put_int((int)(*((uint32_t*)0xc0100000)));
-   int i = 0;
-   for(;i < 10000000;++i){
-   	
-   }
-   put_int((int)(*((uint32_t*)0xc0100000)));
    while(1);
    return 0;
 }
@@ -36,32 +36,32 @@ int main(void) {
 /* 在线程中运行的函数 */
 void k_thread_a(void* arg) {     
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
-   while(1) {
-	console_put_str(" v_a:0x");
-        console_put_int(test_var_a);
-   }
+	console_put_str(" thread_a:");
+        console_put_int(sys_getpid());
+	console_put_str("\n");
+	console_put_str(" prog_a_pid:");
+        console_put_int(prog_a_pid);
+	console_put_str("\n");
+	while(1);
 }
 
 /* 在线程中运行的函数 */
 void k_thread_b(void* arg) {     
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
-   while(1) {
-	console_put_str(" v_b:0x");
-        console_put_int(test_var_b);
-   }
+	console_put_str(" thread_b:");
+        console_put_int(sys_getpid());
+	console_put_str("\n");
+	console_put_str(" prog_b_pid:");
+        console_put_int(prog_b_pid);
+	console_put_str("\n");
+	while(1);
 }
 
 void u_prog_a(void){
-	uint32_t* i;
-	for(i = (uint32_t*)0xc0100000;i < 0xc0100100; ++i){
-		*i = 0;
-	}
-	while(1){
-		//test_var_a++;
-	}
+	prog_a_pid = getpid();
+	while(1);
 }
 void u_prog_b(void){
-	while(1){
-		//test_var_b++;
-	}
+	prog_b_pid = getpid();
+	while(1);
 }
---
+++ c12/a/userprog/syscall-init.c
@@ -0,0 +1,19 @@
+#include "syscall-init.h"
+#include "syscall.h"
+#include "stdint.h"
+#include "print.h"
+#include "thread.h"
+
+#define syscall_nr 32
+typedef void* syscall;
+syscall syscall_table[syscall_nr];
+
+uint32_t sys_getpid(void){
+	return running_thread()->pid;
+}
+
+void syscall_init(void){
+	put_str("syscall init begin\n");
+	syscall_table[SYS_GETPID] = sys_getpid;	
+	put_str("syscall init end\n");
+}
